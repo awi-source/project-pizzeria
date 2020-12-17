8e8c4b75fb2ae717043e53a730eb92dae2181e22
Agnieszka Witek <witek.aga@gmail.com>
2020-12-17 21:06:05 +0100

    Work on home page

---
+++ src/js/components/home.js
@@ -0,0 +1,6 @@
+// document.querySelector('.box-order').addEventListener('click', function(){
+//   console.log('clicked');
+// });
+
+const order = document.querySelector('.box-order');
+console.log(order);
